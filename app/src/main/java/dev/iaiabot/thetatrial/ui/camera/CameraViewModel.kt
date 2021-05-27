4afbaee33a912d59e3bd4f084aa68d011ea9ea69
iaia <iaia72160@gmail.com>
2021-05-27 23:36:05 +0900

    Use camera interface

--- app/src/main/java/dev/iaiabot/thetatrial/ui/camera/CameraViewModel.kt
+++ app/src/main/java/dev/iaiabot/thetatrial/ui/camera/CameraViewModel.kt
@@ -4,9 +4,14 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.iaiabot.thetatrial.theta.Camera
+import dev.iaiabot.thetatrial.theta.CameraImpl
 
 abstract class CameraViewModel : ViewModel(), LifecycleObserver {
     abstract val cameraResponse: LiveData<String>
+    abstract val imageUrl: LiveData<String>
+    abstract val connectedCamera: LiveData<Boolean>
+
     abstract fun onClickConnectCamera()
     abstract fun onClickTakePicture()
 }
@@ -14,10 +19,18 @@ abstract class CameraViewModel : ViewModel(), LifecycleObserver {
 internal class CameraViewModelImpl(
 ) : CameraViewModel() {
     override val cameraResponse = MutableLiveData<String>()
+    override val imageUrl = MutableLiveData<String>()
+    override val connectedCamera = MutableLiveData<Boolean>(false)
+
+    // TODO: diする
+    private val camera: Camera = CameraImpl()
 
     override fun onClickConnectCamera() {
+        camera.connect()
+        connectedCamera.value = true
     }
 
     override fun onClickTakePicture() {
+        imageUrl.value = camera.takePicture()
     }
 }
